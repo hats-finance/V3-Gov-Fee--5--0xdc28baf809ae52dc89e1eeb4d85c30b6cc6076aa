6fd6441899fa29c84cc2ea60d82f9b8d0d55a9df
benk10 <ben.kaufman10@gmail.com>
2024-04-25 11:07:16 +0300

    Remove unused import

--- contracts/tge/HATAirdropFactory.sol
+++ contracts/tge/HATAirdropFactory.sol
@@ -9,7 +9,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IHATAirdrop.sol";
 import "../interfaces/IHATToken.sol";
-import "../tokenlock/HATTokenLock.sol";
 
 contract HATAirdropFactory is Ownable {
     error RedeemDataArraysLengthMismatch();
